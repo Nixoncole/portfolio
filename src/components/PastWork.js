47cd2444721836d5e6eeeadb4d7dcc05c1a5e1c2
Nixoncole <nixontcole@gmail.com>
2019-02-25 21:29:14 -0800

    Finally have things in working order

--- src/components/PastWork.js
+++ src/components/PastWork.js
@@ -6,63 +6,20 @@ class PastWork extends Component {
     constructor () {
         super();
 
-        this.state = {
-            lightboxIsOpen: false,
-            currentImage: 0,
-        };
+    }
 
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
-        this.gotoImage = this.gotoImage.bind(this);
-        this.handleClickImage = this.handleClickImage.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-    }
-    openLightbox (index, event) {
-        event.preventDefault();
-        this.setState({
-            currentImage: index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox () {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious () {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext () {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
-    gotoImage (index) {
-        this.setState({
-            currentImage: index,
-        });
-    }
-    handleClickImage () {
-        if (this.state.currentImage === this.props.images.length - 1) return;
-
-        this.gotoNext();
-    }
-    renderGallery () {
+    renderWork () {
         const { images } = this.props;
 
         if (!images) return;
 
-        const gallery = images.map((obj, i) => {
+        const works = images.map((obj, i) => {
             return (
                 <article className="6u 12u$(xsmall) work-item" key={i}>
                     <a
                         className="image fit thumb"
-                        href={obj.src}
-                        onClick={(e) => this.openLightbox(i, e)}
+                        href={obj.extern}
+                        target="_blank"
                     >
                         <img src={obj.thumbnail} />
                     </a>
@@ -75,23 +32,16 @@ class PastWork extends Component {
 
         return (
             <div className="row">
-                {gallery}
+                {works}
             </div>
         );
     }
     render () {
         return (
             <div>
-                {this.renderGallery()}
+                {this.renderWork()}
                 <Lightbox
-                    currentImage={this.state.currentImage}
                     images={this.props.images}
-                    isOpen={this.state.lightboxIsOpen}
-                    onClickImage={this.handleClickImage}
-                    onClickNext={this.gotoNext}
-                    onClickPrev={this.gotoPrevious}
-                    onClickThumbnail={this.gotoImage}
-                    onClose={this.closeLightbox}
                 />
             </div>
         );
@@ -103,4 +53,4 @@ PastWork.propTypes = {
     images: PropTypes.array
 };
 
-export default PasWork;
+export default PastWork;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,39 +3,47 @@ import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
 // import Lightbox from 'react-images'
-import Gallery from '../components/Gallery'
 import PastWork from '../components/PastWork'
 
-import thumb01 from '../assets/images/thumbs/01.jpg'
-import thumb02 from '../assets/images/thumbs/02.jpg'
-import thumb03 from '../assets/images/thumbs/03.jpg'
-import thumb04 from '../assets/images/thumbs/04.jpg'
-import thumb05 from '../assets/images/thumbs/05.jpg'
-import thumb06 from '../assets/images/thumbs/06.jpg'
-
-import full01 from '../assets/images/fulls/01.jpg'
-import full02 from '../assets/images/fulls/02.jpg'
-import full03 from '../assets/images/fulls/03.jpg'
-import full04 from '../assets/images/fulls/04.jpg'
-import full05 from '../assets/images/fulls/05.jpg'
-import full06 from '../assets/images/fulls/06.jpg'
-
-import favbooknext from '../assets/images/fulls/myfavbook2.png'
 import favbook from '../assets/images/thumbs/myfavbook.png'
+import randstruct from '../assets/images/thumbs/randstruct.png'
+import xpo from '../assets/images/thumbs/xpo.png'
+import apex from '../assets/images/thumbs/apex.png'
+import compli from '../assets/images/thumbs/compli.png'
+import ohsu from '../assets/images/thumbs/ohsu.png'
+import psu from '../assets/images/thumbs/psu.png'
 
-
-const DEFAULT_IMAGES = [
-    { id: '1', src: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '3', src: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '4', src: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '5', src: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '6', src: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
+const MY_WORK = [
+    { id: '1', thumbnail: favbook, caption: 'My-Favorite-Book', extern: "https://github.com/Nixoncole/My-Favorite-Book",
+     description: 'My first Web Application. Inspired by the "Little Free Libraries of Portland", I set out to build an app for book lovers '+
+     'to share their copy of their favorite book, and partake in dicussion with the current reader regardless of where in the world they are.'
+    },
+    { id: '2',thumbnail: randstruct, caption: 'Clang Randstruct', extern: "https://github.com/clang-randstruct",
+     description: 'For my Capstone project, my team was tasked with introducing performance sensitive C structure randomization to the '+
+    "LLVM Clang Compiler. This is our GitHub Organization's page."
+    }
 ];
 
-const MY_IMAGES = [
-    { id: '1', src: favbooknext, thumbnail: favbook, caption: 'My-Favorite-Book', extern: "https://github.com/Nixoncole/My-Favorite-Book", description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', extern: "Somelink", description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+const MY_INTERNSHIPS = [
+    {id:'1', thumbnail: apex, caption:'Apex Clearing', extern: 'https://www.apexclearing.com/', 
+     description:'Functioned within a small team to develop internal tools to aid in development, monitoring, and deployment of software products to '+
+     'the financial tech industry. As a team, researched and implemented first production Kubernetes cluster. Assisted development team in writing '+ 
+     'and deploying cluster friendly applications. Created and modified Helm charts to deploy applications to cluster in a reproducible way. '
+    },
+    {id:'2', thumbnail: compli, caption:'Compli', extern: 'https://www.compli.com/', 
+     description:'Developing APIs in a NodeJS based microservices architecture - currently working on server side rendering of customer compliance '+
+     'and other HR related documents. Debugging and patching a PHP based application that interacts with microservices. Assisting in writing Dockerfiles '+
+     'for new microservices and troubleshooting Kubernetes and Docker related issues around the team.'
+    },
+    {id:'3', thumbnail: xpo, caption:'XPO Logisitics', extern: 'https://www.xpo.com/', 
+     description:'Designed and developed automated test suite to test new internal software before being released. '+
+     'My team developed an automation framework and worked closely with multiple development teams across three different projects HR/Payment '+
+     'related applications. We introduced this framework to other employees and taught them to leverage it to develop their own automated tests.'
+    },
+    {id:'4', thumbnail: ohsu, caption:'OHSU DMICE', extern: 'https://www.ohsu.edu/xd/education/schools/school-of-medicine/departments/clinical-departments/dmice/', 
+     description:'Successfully developed SMART on FHIR applications that could read information from electronic health record databases and perform transformations '+
+    'to the data. Presented technical information and research findings to various work teams, including clinicians, developers, and researchers.'
+    }
 ];
 class HomeIndex extends React.Component {
 
@@ -111,71 +119,35 @@ class HomeIndex extends React.Component {
                     </section>
 
                     <section id="two">
-                        <h2>Recent Work</h2>
-                        <Gallery images={DEFAULT_IMAGES.map(({ id, src, thumbnail, caption, description }) => ({
+                        <h2>Recent Projects</h2>
+                        <PastWork images={MY_WORK.map(({ id, src, thumbnail, caption, extern, description }) => ({
                             src,
                             thumbnail,
                             caption,
+                            extern,
                             description
                         }))} />
 
                         <ul className="actions">
-                            <li><a href="#" className="button">Full Portfolio</a></li>
+                            <li><a href="https://github.com/Nixoncole" target="_blank" className="button">Visit my GitHub</a></li>
                         </ul>
                     </section>
 
                     <section id="three">
-                        <h2>Get In Touch</h2>
-                        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
-                        <div className="row">
-                            <div className="8u 12u$(small)">
-                                <form method="post" action="#">
-                                    <div className="row uniform 50%">
-                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-                                    </div>
-                                </form>
-                                <ul className="actions">
-                                    <li><input type="submit" value="Send Message" /></li>
-                                </ul>
-                            </div>
-                            <div className="4u 12u$(small)">
-                                <ul className="labeled-icons">
-                                    <li>
-                                        <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        1234 Somewhere Rd.<br />
-                                        Nashville, TN 00000<br />
-                                        United States
-                                    </li>
-                                    <li>
-                                        <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        000-000-0000
-                                    </li>
-                                    <li>
-                                        <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                        <a href="#">hello@untitled.tld</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-                     <section id="four">
-                        <h2>Recent Work</h2>
-                        <PastWork images={MY_IMAGES.map(({ id, src, thumbnail, caption, description }) => ({
+                        <h2>Internships</h2>
+                        <PastWork images={MY_INTERNSHIPS.map(({ id, src, thumbnail, caption, extern, description }) => ({
                             src,
                             thumbnail,
                             caption,
+                            extern,
                             description
                         }))} />
 
-
                         <ul className="actions">
-                            <li><a href="#" className="button">Full Portfolio</a></li>
+                            <li><a href="https://www.linkedin.com/in/cole-nixon-181972b8/" target="_blank" className="button">Visit my LinkedIn</a></li>
+                            <li><a href="https://goviks.com/roster.aspx?rp_id=2023" target="_blank"> <img src={psu} height="50" /></a></li>
                         </ul>
                     </section>
-
-
  
                 </div>
 
